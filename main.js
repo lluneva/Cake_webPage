93e89dacfa36d5d555cf5e389ff354f264f22f81
Lidija Luneva <lidiaLuneva@gmail.com>
2018-12-12 13:07:04 +0200

    removed some excess code

--- main.js
+++ main.js
@@ -40,91 +40,67 @@ function showSlides(n) {
 
 }
 
-//chat// - // nestrādā neviens no variantiem. kāpēc? //
-
-$('SendChatMessage').click(function () {
-  
-  var userName = $('#chat-username-input').val();
-  var text = $('#chat-text-input').val();
-  var dateInsert = new Date();
 
 
-  var html = '<div class="chat-message"><h5>' + userName + '<em>' + dateInsert + '</em></h5><p>' + text + '</p></div>';
 
-  $('.chat-main-window').appendTo(html);
+$(document).ready(function () {
 
-  
-});
 
-// document.getElementById("SendChatMessage").addEventListener("click", postMsg);
 
-// function postMsg () {
-//   var userName = document.getElementById("chat-username-input").innerHTML;
-//   var text = document.getElementById("chat-text-input").innerHTML;
-
-//   document.getElementById("newChatMessage") = "YOU CLICKED ME!" ;
-
-// }
-
-$(document).ready(function() {
-  
-
-	  
   var numItems = $('li.fancyTab').length;
-  
-
-      if (numItems === 12){
-        $("li.fancyTab").width('8.3%');
-      }
-      if (numItems === 11){
-        $("li.fancyTab").width('9%');
-      }
-      if (numItems === 10){
-        $("li.fancyTab").width('10%');
-      }
-      if (numItems === 9){
-        $("li.fancyTab").width('11.1%');
-      }
-      if (numItems === 8){
-        $("li.fancyTab").width('12.5%');
-      }
-      if (numItems === 7){
-        $("li.fancyTab").width('14.2%');
-      }
-      if (numItems === 6){
-        $("li.fancyTab").width('16.666666666666667%');
-      }
-      if (numItems === 5){
-        $("li.fancyTab").width('20%');
-      }
-      if (numItems === 4){
-        $("li.fancyTab").width('25%');
-      }
-      if (numItems === 3){
-        $("li.fancyTab").width('33.3%');
-      }
-      if (numItems === 2){
-        $("li.fancyTab").width('50%');
-      }
-    
- 
 
 
-  });
+  if (numItems === 12) {
+    $("li.fancyTab").width('8.3%');
+  }
+  if (numItems === 11) {
+    $("li.fancyTab").width('9%');
+  }
+  if (numItems === 10) {
+    $("li.fancyTab").width('10%');
+  }
+  if (numItems === 9) {
+    $("li.fancyTab").width('11.1%');
+  }
+  if (numItems === 8) {
+    $("li.fancyTab").width('12.5%');
+  }
+  if (numItems === 7) {
+    $("li.fancyTab").width('14.2%');
+  }
+  if (numItems === 6) {
+    $("li.fancyTab").width('16.666666666666667%');
+  }
+  if (numItems === 5) {
+    $("li.fancyTab").width('20%');
+  }
+  if (numItems === 4) {
+    $("li.fancyTab").width('25%');
+  }
+  if (numItems === 3) {
+    $("li.fancyTab").width('33.3%');
+  }
+  if (numItems === 2) {
+    $("li.fancyTab").width('50%');
+  }
 
-$(window).load(function() {
 
-$('.fancyTabs').each(function() {
 
-  var highestBox = 0;
-  $('.fancyTab a', this).each(function() {
-
-    if ($(this).height() > highestBox)
-      highestBox = $(this).height();
-  });
-
-  $('.fancyTab a', this).height(highestBox);
 
 });
-});
 
+$(window).load(function () {
+
+  $('.fancyTabs').each(function () {
+
+    var highestBox = 0;
+    $('.fancyTab a', this).each(function () {
+
+      if ($(this).height() > highestBox)
+        highestBox = $(this).height();
+    });
+
+    $('.fancyTab a', this).height(highestBox);
+
+  });
+});
--- z_footer.php
+++ z_footer.php
@@ -1,18 +1,18 @@
-</div> 
-</div> 
-
-
-<div class="row">
-    <div class="col-sm-12  footer" id="footerelement">
-        &copy; All cake rights baked 2018
-    </div>
+</div>
 </div>
 
+<div class="container">
+    <div class="row">
+        <div class="col footer" id="footerelement">
+            &copy; All cake rights baked 2018
+        </div>
+    </div>
+</div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="main.js"></script>
 
 
